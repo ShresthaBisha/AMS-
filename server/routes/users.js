1d51370f2f96b56860679c46408fb1aba31cbf96
saai <saahithyan123@gmail.com>
2020-06-10 12:57:37 +0530

    routes protected by using middlewares

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,12 +3,14 @@ const bcrypt = require('bcrypt')
 const omit = require('lodash/omit')
 
 const userController = require('../controllers/user')
+const auth = require('../middleware/auth')
+const teacher = require('../middleware/teacher')
 
 const router = express.Router()
 
-router.post('/create', userController.createUser)
-router.post('/search', userController.searchUsers)
-router.get('/read/:id', userController.getUserById)
+router.post('/create', [auth, teacher], userController.createUser)
+router.post('/search', [auth, teacher], userController.searchUsers)
+router.get('/read/:id', auth, userController.getUserById)
 // router.put('/:id/update', userController.user_update)
 // router.delete('/:id/delete', userController.user_delete)
 
