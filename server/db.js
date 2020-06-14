6cddf228ad5a62006ac0bb0abb6e52117c3640e5
Prajwal Gajurel <prajwal@globalwavenet.com>
2020-06-14 09:00:42 +0545

    Updating db file

--- server/db.js
+++ server/db.js
@@ -7,14 +7,14 @@ const {
     MONGO_PORT,
     MONGO_DB
   } = process.env;
-  
+  /*
   const options = {
     useNewUrlParser: true,
     reconnectTries: Number.MAX_VALUE,
     reconnectInterval: 500, 
     connectTimeoutMS: 10000,
   };
-
+*/
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 exports.connectToDatabase =mongoose.connect(url, options).then( function() {
