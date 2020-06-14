f886984f138d6c57cd4dfc0dcf37480872f53842
Prajwal Gajurel <prajwal@globalwavenet.com>
2020-06-14 10:20:38 +0545

    Changed connection url

--- server/db.js
+++ server/db.js
@@ -7,17 +7,17 @@ const {
     MONGO_PORT,
     MONGO_DB
   } = process.env;
-  /*
+  
   const options = {
     useNewUrlParser: true,
     reconnectTries: Number.MAX_VALUE,
     reconnectInterval: 500, 
-    connectTimeoutMS: 10000,
+    connectTimeoutMS: 10000
   };
-*/
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
 
-exports.connectToDatabase =mongoose.connect(url).then( function() {
+//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://db/ams`
+exports.connectToDatabase =mongoose.connect(url, options).then( function() {
   console.log('MongoDB is connected');
 })
   .catch( function(err) {
