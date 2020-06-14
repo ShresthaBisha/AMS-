915369c428ef9fcbf7d30a259d1a767d1a00d79d
Prajwal Gajurel <prajwal@globalwavenet.com>
2020-06-14 08:37:53 +0545

    Added a different db file

--- server/index.js
+++ server/index.js
@@ -12,15 +12,17 @@ const classrooms = require('./routes/classrooms')
 const meetings = require('./routes/meetings')
 const students = require('./routes/students')
 const lectures = require('./routes/lectures')
+const databases =require('./db')
 if (!process.env.JWT_SECRET) {
     console.log('FATAL ERROR: JWT_SECRET is not defined')
     process.exist(1) // 0 means success anything else failure
 }
-
+databases.connectToDatabase
+/*
 mongoose.connect('mongodb://localhost/ams') //:TODO add db username password
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'))
-
+*/
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', users) //:TODO add version
