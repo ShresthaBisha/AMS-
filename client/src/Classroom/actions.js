d7a5ad442459f64bccb4fa8521b814c5d9e8b7a7
Ajay Karki <ajaya.karki30@gmail.com>
2020-06-16 03:41:46 +0530

    add function in classroom's action and source

--- client/src/Classroom/actions.js
+++ client/src/Classroom/actions.js
@@ -17,3 +17,30 @@ export function getClassroomById () {
         })
     }
 }
+
+export function updateClassroomById () {
+    return function (dispactch) {
+        dispactch({
+            type: 'UPDATE_CLASSROOM_BY_ID',
+            payload: source.updateClassroomById(),
+        })
+    }
+}
+
+export function deleteClassroom () {
+    return function (dispactch) {
+        dispactch({
+            type: 'DELETE_CLASSROOM_BY_ID',
+            payload: source.deleteClassroom(),
+        })
+    }
+}
+
+export function getAllClassroom () {
+    return function (dispactch) {
+        dispactch({
+            type: 'GET_ALL_CLASSROOM_BY_ID',
+            payload: source.getAllClassroom(),
+        })
+    }
+}
--- client/src/Classroom/source.js
+++ client/src/Classroom/source.js
@@ -7,3 +7,15 @@ export const addClassroom = auth => {
 export const getClassroomById = auth => {
     return request.get('classrooms/5ededf8815518341543fffe8', auth)
 }
+
+export const updateClassroomById = auth => {
+    return request.put('classrooms/5edeee65807c6725682d190d/update', auth)
+}
+
+export const deleteClassroom = auth => {
+    return request.delete('classrooms/5edeee65807c6725682d190d/delete', auth)
+}
+
+export const getAllClassroom = auth => {
+    return request.get('classrooms/', auth)
+}
