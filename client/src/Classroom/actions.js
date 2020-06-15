4e0418cd3dfe2ff78fd560581d0c7ffd361e35f5
Ajay Karki <ajaya.karki30@gmail.com>
2020-06-15 15:19:54 +0530

    add classroom action and source

--- client/src/Classroom/actions.js
+++ client/src/Classroom/actions.js
@@ -1,10 +1,19 @@
-import { addClassroom } from "./source";
+import * as source from './source'
 
-export function addClassroomAction() {
-  return function (dispactch) {
-    dispactch({
-      type: "ADD_CLASSROOM_DETAILS",
-      payload: addClassroom(),
-    });
-  };
+export function addClassroom () {
+    return function (dispactch) {
+        dispactch({
+            type: 'ADD_CLASSROOM',
+            payload: source.addClassroom(),
+        })
+    }
+}
+
+export function getClassroomById () {
+    return function (dispactch) {
+        dispactch({
+            type: 'GET_CLASSROOM_BY_ID',
+            payload: source.getClassroomById(),
+        })
+    }
 }
