161c43e6676ccc04c9d4a46077659d5196a81002
Ajay Karki <ajaya.karki30@gmail.com>
2020-06-16 04:23:09 +0530

    add students' action and source file

--- client/src/Student/actions.js
+++ client/src/Student/actions.js
@@ -1,19 +1,49 @@
-import { getEmployeeDetails, addEmployeeDetails } from "./source";
+import * as source from './source'
 
-export function getEmployeeDetailsAction() {
-  return function (dispactch) {
-    dispactch({
-      type: "GET_EMPLOYEE_DETAILS",
-      payload: getEmployeeDetails(),
-    });
-  };
+export function addStudent () {
+    return function (dispatch) {
+        dispatch({
+            type: 'ADD_STUDENT',
+            payload: source.addStudent(),
+        })
+
+    }
 }
 
-export function addEmployeeDetailsAction() {
-  return function (dispactch) {
-    dispactch({
-      type: "ADD_EMPLOYEE_DETAILS",
-      payload: addEmployeeDetails(),
-    });
-  };
+export function searchStudent () {
+    return function (dispactch) {
+        dispactch({
+            type: 'SEARCH_STUDENT',
+            payload: source.searchStudent(),
+        })
+    }
 }
+
+export function getStudentById () {
+    return function (dispactch) {
+        dispactch({
+            type: 'GET_STUDENT_BY_ID',
+            payload: source.getStudentById(),
+        })
+    }
+}
+
+// import { getEmployeeDetails, addEmployeeDetails } from "./source";
+
+// export function getEmployeeDetailsAction() {
+//   return function (dispactch) {
+//     dispactch({
+//       type: "GET_EMPLOYEE_DETAILS",
+//       payload: getEmployeeDetails(),
+//     });
+//   };
+// }
+//
+// export function addEmployeeDetailsAction() {
+//   return function (dispactch) {
+//     dispactch({
+//       type: "ADD_EMPLOYEE_DETAILS",
+//       payload: addEmployeeDetails(),
+//     });
+//   };
+// }
