7c7d53749b93ea4a29c6a6507f3f1595b392f752
Ajay Karki <ajaya.karki30@gmail.com>
2020-06-16 17:27:13 +0530

    add reducer

--- client/src/Classroom/reducer.js
+++ client/src/Classroom/reducer.js
@@ -5,34 +5,47 @@ const initialState = {
     addState: 0,
     fetchState: 0,
 
-
-
     updateState: 0,
+    deleteState: 0,
     error: {},
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'ADD_CLASSROOM_PENDING':
-            return { ...state, addState: 1 }
         case 'ADD_CLASSROOM_FULFILLED':
             return { ...state, addState: 2, classroom: action.payload.data }
         case 'ADD_CLASSROOM_REJECTED':
             return { ...state, addState: 3 }
+        case 'ADD_CLASSROOM_PENDING':
+            return { ...state, addState: 1 }
 
-        case 'GET_CLASSROOM_BY_ID_PENDING':
-            return { ...state, fetchState: 1 }
         case 'GET_CLASSROOM_BY_ID_FULFILLED':
             return { ...state, fetchState: 2, classroom: action.payload.data }
         case 'GET_CLASSROOM_BY_ID_REJECTED':
             return { ...state, fetchState: 3 }
-
-        case 'GET_ALL_CLASSROOM_PENDING':
+        case 'GET_CLASSROOM_BY_ID_PENDING':
             return { ...state, fetchState: 1 }
+
+        case 'UPDATE_CLASSROOM_FULFILLED':
+            return { ...state, updateState: 2, classroom: action.payload.data }
+        case 'UPDATE_CLASSROOM_REJECTED':
+            return { ...state, updateState: 3 }
+        case 'UPDATE_CLASSROOM_PENDING':
+            return { ...state, updateState: 1 }
+
+        case 'DELETE_CLASSROOM_FULFILLED':
+            return { ...state, deleteState: 2, classroom: action.payload.data }
+        case 'DELETE_CLASSROOM_REJECTED':
+            return { ...state, deleteState: 3 }
+        case 'DELETE_CLASSROOM_PENDING':
+            return { ...state, deleteState: 1 }
+
         case 'GET_ALL_CLASSROOM_FULFILLED':
             return { ...state, fetchState: 2, classrooms: action.payload.data }
         case 'GET_ALL_CLASSROOM_REJECTED':
             return { ...state, fetchState: 3 }
+        case 'GET_ALL_CLASSROOM_PENDING':
+            return { ...state, fetchState: 1 }
 
         default:
             return state
--- client/src/Student/actions.js
+++ client/src/Student/actions.js
@@ -27,23 +27,3 @@ export function getStudentById () {
         })
     }
 }
-
-// import { getEmployeeDetails, addEmployeeDetails } from "./source";
-
-// export function getEmployeeDetailsAction() {
-//   return function (dispactch) {
-//     dispactch({
-//       type: "GET_EMPLOYEE_DETAILS",
-//       payload: getEmployeeDetails(),
-//     });
-//   };
-// }
-//
-// export function addEmployeeDetailsAction() {
-//   return function (dispactch) {
-//     dispactch({
-//       type: "ADD_EMPLOYEE_DETAILS",
-//       payload: addEmployeeDetails(),
-//     });
-//   };
-// }
